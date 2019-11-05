67adb992e5ec008bb19df4cade06de88308bf780
Courtland Bourgeois <courtland@CourtlandsMBP2.eatel.net>
2019-11-05 15:18:03 -0600

    completed all of MVP but the styling of the buttons

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = (props) => {
+
+  const [displayNumbers] = useState(0);
+
+  return (
+    <div className="display">
+      {/* Display any props data here */}
+
+      <p>{displayNumbers}</p>
+
+    </div>
+  )
 };
+
+export default Display;
